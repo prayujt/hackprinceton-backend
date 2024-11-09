30a43c276d8d06876a477949f76618377a4dd6a8
Prayuj Tuli <prayujtuli@hotmail.com>
2024-11-09 14:40:27 -0500

    added database query boilerplate

---
+++ database/database.go
@@ -0,0 +1,72 @@
+package database
+
+import (
+	"database/sql"
+	"reflect"
+	"strings"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+var db *sql.DB
+
+func Query[T any](arr *[]T, query_ string, args ...any) {
+	rows, err := db.Query(query_, args...)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	structType := reflect.TypeOf(arr).Elem().Elem()
+
+	var fields []string
+	cols, err := rows.Columns()
+	for _, col := range cols {
+		found := false
+		for i := 0; i < structType.NumField(); i++ {
+			if strings.EqualFold(structType.Field(i).Name, col) || strings.EqualFold(structType.Field(i).Tag.Get("database"), col) {
+				fields = append(fields, structType.Field(i).Name)
+				found = true
+			}
+		}
+		if !found {
+			fields = append(fields, "")
+		}
+	}
+
+	for rows.Next() {
+		newStruct := reflect.New(structType).Elem()
+		var properties []interface{}
+
+		for _, field := range fields {
+			if field == "" {
+				var nothing interface{}
+				properties = append(properties, &nothing)
+			} else {
+				properties = append(properties, newStruct.FieldByName(field).Addr().Interface())
+			}
+		}
+
+		rows.Scan(properties...)
+		*arr = append(*arr, newStruct.Interface().(T))
+	}
+}
+
+func QueryValue[T any](value *T, query_ string, args ...any) error {
+	err := db.QueryRow(query_, args...).Scan(value)
+	return err
+}
+
+func Execute(exec string, args ...any) (sql.Result, error) {
+	result, err := db.Exec(exec, args...)
+	return result, err
+}
+
+func InitDatabase(dbUrl string) {
+	var err error
+	db, err = sql.Open("pgx", dbUrl)
+
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	_ "github.com/jackc/pgx/v5/stdlib"
+
+	"hackprinceton/database"
+)
+
+func main() {
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		log.Fatal("DATABASE_URL must be set")
+	}
+
+	environment := os.Getenv("ENVIRONMENT")
+	if environment == "" {
+		environment = "development"
+	}
+
+	database.InitDatabase(databaseUrl)
+	log.Println("Connected to database")
+
+	r := mux.NewRouter()
+
+	fmt.Println("Server running on 0.0.0.0:8080")
+
+	if environment == "development" {
+		corsMiddleware := handlers.CORS(
+			handlers.AllowedOrigins([]string{"http://localhost:5173", "http://localhost:4173"}),
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+			handlers.AllowedHeaders([]string{"Content-Type", "Authorization", "Cookie"}),
+			handlers.AllowCredentials(),
+		)
+		log.Fatal(http.ListenAndServe(":8080", corsMiddleware(r)))
+	} else {
+		log.Fatal(http.ListenAndServe(":8080", r))
+	}
+}
