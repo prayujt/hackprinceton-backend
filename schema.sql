2d61245a774ab82e4fde564cd75b71d5e201df2e
Prayuj Tuli <prayujtuli@hotmail.com>
2024-11-09 15:01:56 -0500

    added sql schema

---
+++ schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE sets (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    author_id UUID NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE cards (
+    id UUID PRIMARY KEY,
+    set_id UUID NOT NULL,
+    front JSON NOT NULL,
+    back JSON NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (set_id) REFERENCES sets(id)
+);
+
+CREATE TABLE stats (
+    user_id UUID NOT NULL,
+    card_id UUID NOT NULL,
+    seen_count INT DEFAULT 0,
+    answered_count INT DEFAULT 0,
+    correct_count INT DEFAULT 0,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    PRIMARY KEY (user_id, card_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (card_id) REFERENCES cards(id)
+);
